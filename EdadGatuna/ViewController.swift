cee10305e6ec5a0ede001abb240753df4062794e
Luis Enrique Tierrafria Rodriguez <luis@Luiss-iMac.lan>
2016-07-24 14:59:25 -0500

    primera app Swift

--- EdadGatuna/ViewController.swift
+++ EdadGatuna/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var age: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func findAge(sender: AnyObject) {
+        let humanAge = Int(age.text!)
+        if humanAge != nil {
+            let catYears = humanAge! * 7
+            print(catYears)
+            result.text = "Tu gato tiene \(catYears) años gatunos"
+        }
+        else{
+            result.text = "Ingresa la edad de tu gato"
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+   
 
 }
 
